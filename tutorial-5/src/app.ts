2b886cbd918c327b7e45fb2e05b77fe1f413ef9e
Alex Petrov <a.petrov@open-s.info>
2026-04-04 20:14:44 +0500

    The way to attach the app to a canvas.

--- tutorial-5/src/app.ts
+++ tutorial-5/src/app.ts
@@ -7,10 +7,12 @@ export class App {
     private renderer!: Renderer;
     private mesh!: Mesh;
 
-    async Run() {
-        this.mesh = await AssetLoader.loadMesh("assets/models/vertices.json", "assets/models/faces.json", "assets/textures/texture.png");
-        const canvas = <HTMLCanvasElement>document.querySelector("canvas");
-
+    async Run(canvas: HTMLCanvasElement) {
+        this.mesh = await AssetLoader.loadMesh(
+            "assets/models/vertices.json",
+            "assets/models/faces.json",
+            "assets/textures/texture.png");
+            
         try {
             this.renderer = await Renderer.create(
                 canvas,
