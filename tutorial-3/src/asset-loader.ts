b1ee0ba7f8d9656e491ce8c72c6ec6eaf1552143
Alex Petrov <a.petrov@open-s.info>
2026-06-22 18:57:32 +0500

    Fixed it here too.

--- tutorial-3/src/asset-loader.ts
+++ tutorial-3/src/asset-loader.ts
@@ -2,20 +2,25 @@ import { Mesh } from "./mesh.js";
 import { Vertex } from "./vertex.js";
 
 export class AssetLoader {
-        static async loadMesh(verticesFilename: string): Promise<Mesh> {
-            const vertices: Vertex[] = await this.loadVertices(verticesFilename);
-            return new Mesh(vertices);
-        }
+    static async loadMesh(url: string): Promise<Mesh> {
+        const vertices: Vertex[] = await this.loadVertices(url);
+        return new Mesh(vertices);
+    }
 
-        private static async loadVertices(filename: string): Promise<Vertex[]> {
-            const response = await fetch(filename);
-            const vertices: Vertex[] = await response.json();
-            return vertices;
-        }
-    
-        static async loadShader(filename: string): Promise<string> {
-            const file = await fetch(filename);
-            const text = await file.text();
-            return text;
-        }
+    private static async loadVertices(url: string): Promise<Vertex[]> {
+        const response = await fetch(AssetLoader.resolvePath(url));
+        const vertices: Vertex[] = await response.json();
+        return vertices;
+    }
+
+    static async loadShader(url: string): Promise<string> {
+        const file = await fetch(AssetLoader.resolvePath(url));
+        const text = await file.text();
+        return text;
+    }
+
+    private static resolvePath(url: string) {
+        const result = new URL(url, import.meta.url).href;
+        return result;
+    }
 }
