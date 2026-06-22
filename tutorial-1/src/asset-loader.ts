3af31d2e4ea26c8e969b4dbe468ac26dcb1ebb6e
Alex Petrov <a.petrov@open-s.info>
2026-06-22 17:58:30 +0500

    Adjustments.

--- tutorial-1/src/asset-loader.ts
+++ tutorial-1/src/asset-loader.ts
@@ -2,15 +2,20 @@ import { Geometry } from "./geometry.js";
 import { Vertex } from "./vertex.js";
 
 export class AssetLoader {
-        static async loadGeometry(filename: string): Promise<Geometry> {
-            const response = await fetch(filename);
-            const vertices: Vertex[] = await response.json();
-            return new Geometry(vertices);
-        }
-    
-        static async loadShader(filename: string): Promise<string> {
-            const file = await fetch(filename);
-            const text = await file.text();
-            return text;
-        }
+    static async loadGeometry(url: string): Promise<Geometry> {
+        const response = await fetch(AssetLoader.resolvePath(url));
+        const vertices: Vertex[] = await response.json();
+        return new Geometry(vertices);
+    }
+
+    static async loadShader(url: string): Promise<string> {
+        const file = await fetch(AssetLoader.resolvePath(url));
+        const text = await file.text();
+        return text;
+    }
+
+    private static resolvePath(url: string) {
+        const result = new URL(url, import.meta.url).href;
+        return result;
+    }
 }
