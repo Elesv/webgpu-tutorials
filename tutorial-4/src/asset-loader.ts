7b62a65eddbbca9324718131b14363382fe0f4b4
Alex Petrov <a.petrov@open-s.info>
2026-06-22 19:21:09 +0500

    Another fix.

--- tutorial-4/src/asset-loader.ts
+++ tutorial-4/src/asset-loader.ts
@@ -3,27 +3,32 @@ import { Mesh } from "./mesh.js";
 import { Vertex } from "./vertex.js";
 
 export class AssetLoader {
-        static async loadMesh(verticesFilename: string, facesFilename: string): Promise<Mesh> {
-            const vertices: Vertex[] = await this.loadVertices(verticesFilename);
-            const faces: Face[] = await this.loadFaces(facesFilename);
-            return new Mesh(vertices, faces);
-        }
+    static async loadMesh(verticesUrl: string, facesUrl: string): Promise<Mesh> {
+        const vertices: Vertex[] = await this.loadVertices(verticesUrl);
+        const faces: Face[] = await this.loadFaces(facesUrl);
+        return new Mesh(vertices, faces);
+    }
 
-        private static async loadVertices(filename: string): Promise<Vertex[]> {
-            const response = await fetch(filename);
-            const vertices: Vertex[] = await response.json();
-            return vertices;
-        }
+    private static async loadVertices(url: string): Promise<Vertex[]> {
+        const response = await fetch(AssetLoader.resolvePath(url));
+        const vertices: Vertex[] = await response.json();
+        return vertices;
+    }
 
-        private static async loadFaces(filename: string): Promise<Face[]> {
-            const response = await fetch(filename);
-            const faces: Face[] = await response.json();
-            return faces;
-        }
-    
-        static async loadShader(filename: string): Promise<string> {
-            const file = await fetch(filename);
-            const text = await file.text();
-            return text;
-        }
+    private static async loadFaces(url: string): Promise<Face[]> {
+        const response = await fetch(AssetLoader.resolvePath(url));
+        const faces: Face[] = await response.json();
+        return faces;
+    }
+
+    static async loadShader(url: string): Promise<string> {
+        const file = await fetch(AssetLoader.resolvePath(url));
+        const text = await file.text();
+        return text;
+    }
+
+    private static resolvePath(url: string) {
+        const result = new URL(url, import.meta.url).href;
+        return result;
+    }
 }
