29bc3c1022bf95fc0fd2d4ce454ea6ecea29bfc9
Alex Petrov <a.petrov@open-s.info>
2026-07-02 04:49:21 +0500

    Some fixes here and there.

--- tutorial-4/src/renderer.ts
+++ tutorial-4/src/renderer.ts
@@ -183,7 +183,7 @@ export class Renderer {
             colorAttachments: [
                 {
                     view: view,
-                    clearValue: { r: 0.0, g: 0.0, b: 1.0, a: 1.0 },
+                    clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 0.0 },
                     loadOp: "clear",
                     storeOp: "store"
                 }
--- tutorial-4/src/shaders/shaders.wgsl
+++ tutorial-4/src/shaders/shaders.wgsl
@@ -7,5 +7,5 @@ fn vs_main(@location(0) position: vec3<f32>) -> @builtin(position) vec4<f32>
 @fragment
 fn fs_main() -> @location(0) vec4<f32>
 {
-    return vec4(1, 0, 0, 1);
+    return vec4(1, 1, 1, 1);
 }
